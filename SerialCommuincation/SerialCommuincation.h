eafd43919c0377a5c0f214d02be8e7da38318064
Krishnaprasad KV <163698680+krishnakv24@users.noreply.github.com>
2024-03-21 20:20:27 +0400

    -Initial

---
+++ SerialCommuincation/SerialCommuincation.h
@@ -0,0 +1,32 @@
+
+// SerialCommuincation.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CSerialCommuincationApp:
+// See SerialCommuincation.cpp for the implementation of this class
+//
+
+class CSerialCommuincationApp : public CWinApp
+{
+public:
+	CSerialCommuincationApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSerialCommuincationApp theApp;
